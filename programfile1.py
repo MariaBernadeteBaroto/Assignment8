c1926ee6dc92392b44e7d2ad8d92f3cead7aa3cd
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-12-04 01:38:21 -0800

    program1 steps (flow)

---
+++ programfile1.py
@@ -0,0 +1,8 @@
+# Program 1. Lottery
+# steps
+# ask for 3 numbers (0-9)
+# Display 'winner' if all 3 input numbers matched the generated numbers
+# Display 'You loss' if not
+# Dislay 'Try again y/n'
+# if the user enter 'y' the user will play again
+# if 'n' the program will exit
