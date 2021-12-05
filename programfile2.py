be3dc6ed65ca44ba8e05184288b80dd3653bde27
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-12-05 06:12:33 -0800

    guess the number program done

--- programfile2.py
+++ programfile2.py
@@ -7,7 +7,6 @@ import random
 
 def guess_number():
     random_number = random.randint(0,100)  # Generate 1 random number (0-100)
-    print(random_number)
     guess = int(input('Enter your guess number: ')) # Ask the user to guess the number
     while guess != random_number:
         if guess > random_number:   # Display "Greater than" if the inputed number is greater than the random number
@@ -20,10 +19,3 @@ def guess_number():
     print("You've got it!")
 
 guess_number()
-
-
-# Generate 1 random number (0-100)
-# Ask the user to guess the number
-# Display "Greater than" if the inputed number is greater than the random number
-# Display "Less than" if the inputed number is less than the random number
-# Repeat asking the user until the random number has been guessed correctly.
