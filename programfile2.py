baa8c612d54fd2bd3045a392e2a3b6315555e5ee
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-12-05 06:03:52 -0800

    if statements under while condition done

--- programfile2.py
+++ programfile2.py
@@ -6,6 +6,15 @@ def guess_number():
     random_number = random.randint(0,100)  # Generate 1 random number (0-100)
     guess = int(input('Enter your guess number: ')) # Ask the user to guess the number
     while guess != random_number:
+        if guess > random_number:   # Display "Greater than" if the inputed number is greater than the random number
+            print('Greater than') 
+            guess = int(input('Enter another guess number: '))
+        if guess < random_number:
+            print('Less than')
+            guess = int(input('Enter another guess number: '))
+    print("You've got it!")
+
+guess_number()
 
 
 # Generate 1 random number (0-100)
