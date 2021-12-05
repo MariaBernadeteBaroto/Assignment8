41bef4b961042b26a1a4f0e12bd33c0b4d822e10
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2021-12-05 04:19:30 -0800

    add condition on while statement

--- programfile1.py
+++ programfile1.py
@@ -1,6 +1,20 @@
 # Program 1. Lottery
 # steps
+
+print('Welcome to the game!')
+print('Lets start!')
+
 # ask for 3 numbers (0-9)
+import random
+
+
+def lottery():
+    more_game = 'yes'
+    while more_game[0] == 'y':
+    
+
+
+
 # Display 'winner' if all 3 input numbers matched the generated numbers
 # Display 'You loss' if not
 # Dislay 'Try again y/n'
