1b15f88eb479adfbd2f9ee57b8865e81b0576289
Ma. Bernadete Baroto <bernadetebaroto@gmail.com>
2022-01-12 02:40:10 -0800

    program to prevent duplicates on user input

--- programfile1.py
+++ programfile1.py
@@ -14,22 +14,27 @@ def lottery():
 
         list_numbers = [0,1,2,3,4,5,6,7,8,9]
         random_numbers = random.sample(list_numbers, 3)  # generated three(3) random numbers
+        print(random_numbers)
+        
+        guess_numbers = []
 
-        print("Do not repeat input numbers")
-        number1 = int(input('Enter first number: '))
-        number2 = int(input('Enter second number: '))
-        number3 = int(input('Enter three number: '))
+        while len(guess_numbers) < 3:
+            UserInput = int(input("Enter a number: "))
+            if UserInput not in guess_numbers:
+                UserInput = guess_numbers.append(UserInput)
+                print(guess_numbers)
+            else:
+                print("You already use this number, try to use another one (0-9): ")
+            
 
-        numbers = (number1, number2, number3)    # ask for three numbers
-
-        def count_char(numbers, random_numbers):
+        def count_char(guess_numbers, random_numbers):
             count = 0
             for c in random_numbers:
-                for d in numbers:
+                for d in guess_numbers:
                     if c == d:
                         count = count + 1
             return count
-        result = count_char(numbers, random_numbers)
+        result = count_char(guess_numbers, random_numbers)
 
         if result == 3:
             print('Winner!')    # Display 'winner' if all 3 input numbers matched the generated numbers
